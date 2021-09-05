915a0f87d800bee283d177b6b4b60e20736d0e63
Andrew Khan <andre-khan@yandex.ru>
2021-09-05 19:41:11 +0300

    Домашняя работа 05.09.21 android studio (Создать три блока с TextView  и сгруперовать их по вертикали.  Изменить содержимое TextView при загрузки приложения.)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        TextView textView = (TextView) findViewById(R.id.header);
+        textView.setText("Hello");
+
+        TextView textView2 = (TextView) findViewById(R.id.header2);
+        textView.setText("from");
+
+        TextView textView3 = (TextView) findViewById(R.id.header3);
+        textView.setText("Java!!!");
     }
 }
